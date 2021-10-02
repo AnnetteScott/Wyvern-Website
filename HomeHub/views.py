e713e9c52914e6b794c32b806c8c9046ff72b354
NotNatural21 <annettemorganscott@outlook.co.nz>
2021-10-03 08:58:25 +1300

    Added Templates

--- HomeHub/views.py
+++ HomeHub/views.py
@@ -1,8 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the Home Page <br> Sadly there is nothing here becuase we don't know how")
+    return render(request, 'HomeHub/home.html')
