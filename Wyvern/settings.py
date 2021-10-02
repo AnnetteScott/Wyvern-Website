9fd2d58a45a917afc7336e5036a82c681b0eec22
NotNatural21 <annettemorganscott@outlook.co.nz>
2021-10-02 22:43:22 +1300

    Revert "Update settings.py"
    This reverts commit 7fc60d32a06c96c54f8445366533c934d6aeb14b.

--- Wyvern/settings.py
+++ Wyvern/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,7 +114,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
@@ -133,5 +131,3 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
