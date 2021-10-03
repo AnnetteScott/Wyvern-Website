7ee6be007f7e13e2d23f03d2c1bca8b75a696750
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-03 18:41:08 +1300

    Created Angle function

--- ToWay/functions.py
+++ ToWay/functions.py
@@ -1,3 +1,9 @@
 import random
 def random_way(num_way):
     output = random.randrange(1, (num_way + 1))
+    return output
+
+def angle_spin(num_way, selected_way):
+    offset = -90
+    base = int(180 / num_way)
+    angle = (base * selected_way) - offset
