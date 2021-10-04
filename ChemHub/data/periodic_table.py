a03095d194abb39b43e73d2c9b329f0a2443858d
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-04 15:45:45 +1300

    Added H, He to data

---
+++ ChemHub/data/periodic_table.py
@@ -0,0 +1,24 @@
+periodic_table = [
+  {
+    "atomic": 1,
+    "name": "Hydrogen",
+    "mass": "1.008",
+    "symbol": "H",
+    "state": "colorless gas",
+    "melting": "13.99",
+    "boling": "20.271",
+    "density": "0.08988",
+    "electron": "1s1",
+  },
+  {
+    "atomic": 2,
+    "name": "Helium",
+    "mass": "4.002602",
+    "symbol": "He",
+    "state": "colorless gas",
+    "melting": "0.95 ",
+    "boling": "4.222 ",
+    "density": "0.1786 ",
+    "electron": "1s2",
+  },
+]
