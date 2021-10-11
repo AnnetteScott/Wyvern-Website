b991c4d2be56ea2db95e0e62b4cab55148213ce7
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-11 21:47:31 +1300

    Update functions.py
    Finished functions

--- CookieBook/functions.py
+++ CookieBook/functions.py
@@ -6,40 +6,46 @@ def readfile(filename):
     return file_list
 
 
-def split_recipes(recipe_list):
+def split_recipes(recipe_raw):
     title = ""
     ingred = []
     cooking_time = ""
     instructions = []
     recipe = []
-    for index in range(0, len(recipe_list)):
-        if "Title:" in recipe_list[index]:
-            semi = recipe_list[index].find(":") + 1
-            title = recipe_list[index][semi:]
+    recipe_list = []
+    for index in range(0, len(recipe_raw)):
+        if "Title:" in recipe_raw[index]:
+            semi = recipe_raw[index].find(":") + 1
+            title = recipe_raw[index][semi:]
             title = title.strip()
             recipe.append(title)
-        elif "Ingredients:" in recipe_list[index]:
+        elif "Ingredients:" in recipe_raw[index]:
             ingredient_index = index  + 1
-            ingredient_line = recipe_list[ingredient_index]
+            ingredient_line = recipe_raw[ingredient_index]
             while "-" in ingredient_line:
-                dash = recipe_list[ingredient_index].find("-") + 1
-                ingredient = recipe_list[ingredient_index][dash:]
+                dash = recipe_raw[ingredient_index].find("-") + 1
+                ingredient = recipe_raw[ingredient_index][dash:]
                 ingredient = ingredient.strip()               
                 ingred.append(ingredient)
             recipe.append(ingred)
-        elif "Cooking Time:" in recipe_list[index]:
-            semi = recipe_list[index].find(":") + 1
-            cooking_time = recipe_list[index][semi:]
+        elif "Cooking Time:" in recipe_raw[index]:
+            semi = recipe_raw[index].find(":") + 1
+            cooking_time = recipe_raw[index][semi:]
             cooking_time = cooking_time.strip()
             recipe.append(cooking_time)
-        elif "Instructions:" in recipe_list[index]:
+        elif "Instructions:" in recipe_raw[index]:
             instructions_index = index  + 1
-            instructions_line = recipe_list[instructions_index]
+            instructions_line = recipe_raw[instructions_index]
             while "-" in instructions_line:
-                dash = recipe_list[instructions_index].find("-") + 1
-                instruction = recipe_list[instructions_index][dash:]
+                dash = recipe_raw[instructions_index].find("-") + 1
+                instruction = recipe_raw[instructions_index][dash:]
                 instruction = instruction.strip()               
                 instructions.append(instruction)
             recipe.append(instructions)
+        recipe_tuple = tuple(recipe)
+        recipe_list.append(recipe_tuple)
+    return recipe_list
+
+    
     
 
