042c06b5c66adbaafeef9b497e8409c6d71aef0f
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-13 14:51:02 +1300

    Updated colours

--- CookieBook/views.py
+++ CookieBook/views.py
@@ -12,30 +12,30 @@ def home(request):
 def home(request):
     return render(request, 'CookieBook/home.html')
 
-recipes = functions.get_recipe_dict_cooking()
+recipes_cooking = functions.get_recipe_dict_cooking()
 
 def cooking(request):
-    context = {'recipes': recipes}
+    context = {'recipes': recipes_cooking}
     return render(request, 'CookieBook/cooking.html', context)
 
 def cookingRecipe(request, recipename):
     recipe = None
-    for pair in recipes:
+    for pair in recipes_cooking:
         if pair['url'] == str(recipename):
             recipe = pair
     context = {'recipe': recipe}
     return render(request, 'CookieBook/recipe.html', context)
 
 
-recipes = functions.get_recipe_dict_baking()
+recipes_baking = functions.get_recipe_dict_baking()
   
 def baking(request):
-    context = {'recipes': recipes}
+    context = {'recipes': recipes_baking}
     return render(request, 'CookieBook/baking.html', context)
 
 def bakingRecipe(request, recipename):
     recipe = None
-    for pair in recipes:
+    for pair in recipes_baking:
         if pair['url'] == str(recipename):
             recipe = pair
     context = {'recipe': recipe}
