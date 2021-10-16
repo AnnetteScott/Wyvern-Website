3434cfce85c0228cb0a87f9d7b3a406d2e6991b8
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-16 19:42:50 +1300

    Fixed having to reboot the server

--- CookieBook/views.py
+++ CookieBook/views.py
@@ -6,13 +6,13 @@ from . import functions
 def home(request):
     return render(request, 'CookieBook/home.html')
 
-recipes_cooking = functions.get_recipe_dict("Cooking")
-
 def cooking(request):
+    recipes_cooking = functions.get_recipe_dict("Cooking")
     context = {'recipes': recipes_cooking}
     return render(request, 'CookieBook/cooking.html', context)
 
 def cookingRecipe(request, recipename):
+    recipes_cooking = functions.get_recipe_dict("Cooking")
     recipe = None
     for pair in recipes_cooking:
         if pair['url'] == str(recipename):
@@ -20,14 +20,14 @@ def cookingRecipe(request, recipename):
     context = {'recipe': recipe}
     return render(request, 'CookieBook/recipe.html', context)
 
-
-recipes_baking = functions.get_recipe_dict("Baking")
   
 def baking(request):
+    recipes_baking = functions.get_recipe_dict("Baking")
     context = {'recipes': recipes_baking}
     return render(request, 'CookieBook/baking.html', context)
 
 def bakingRecipe(request, recipename):
+    recipes_baking = functions.get_recipe_dict("Baking")
     recipe = None
     for pair in recipes_baking:
         if pair['url'] == str(recipename):
