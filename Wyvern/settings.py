793988c6e88525b088ab369cf1b05754aea4445d
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-18 17:22:12 +1300

    Update settings.py

--- Wyvern/settings.py
+++ Wyvern/settings.py
@@ -63,9 +63,7 @@ ROOT_URLCONF = 'Wyvern.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
