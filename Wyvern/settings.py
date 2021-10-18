a148395f7d3f1288f380b726c6a714377fd38ce4
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-18 17:48:49 +1300

    Update settings.py

--- Wyvern/settings.py
+++ Wyvern/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, "HomeHub", "templates", "homehub")
+            os.path.join(BASE_DIR, "HomeHub/templates",)
         
         ],
         'APP_DIRS': True,
