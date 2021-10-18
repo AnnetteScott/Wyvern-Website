de744df846e85c434f0a85e1bd11e0d03a116c1c
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-18 17:52:31 +1300

    Update views.py

--- HomeHub/views.py
+++ HomeHub/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'homehub/home.html')
+    return render(request, 'HomeHub/home.html')
