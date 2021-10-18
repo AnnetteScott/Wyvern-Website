41b5ecf417d483c295f0daf3df83a38ffa63f520
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-19 08:39:05 +1300

    Update settings.py
    Removed secret key and changed it

--- Wyvern/settings.py
+++ Wyvern/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lq4e(b74l$5q_93gkaxyi24o96+^x_)+or#ceo#$e5yo=y5fgn'
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
