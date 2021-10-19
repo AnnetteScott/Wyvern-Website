37816fb748ec9161738894f89ad5e1978b038c5b
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-20 10:54:48 +1300

    Update models.py

--- ChemHub/models.py
+++ ChemHub/models.py
@@ -6,11 +6,20 @@ class Element(models.Model):
     atomic_number = models.CharField(max_length=50)
     mass = models.CharField(max_length=50)
     symbol = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    melting = models.CharField(max_length=50)
-    boiling = models.CharField(max_length=50)
-    density = models.CharField(max_length=50)
     electron = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    melting = models.CharField(max_length=50, null=True, blank=True)
+    boiling = models.CharField(max_length=50, null=True, blank=True)
+    sublimation = models.CharField(max_length=50, null=True, blank=True)
+    density_at_stp = models.CharField(max_length=50, null=True, blank=True)
+    density_at_rt = models.CharField(max_length=50, null=True, blank=True)
+    density_at_mp = models.CharField(max_length=50, null=True, blank=True)
+    triple_point = models.CharField(max_length=50, null=True, blank=True)
+    critical_point = models.CharField(max_length=50, null=True, blank=True)
+    heat_capacity = models.CharField(max_length=50, null=True, blank=True)
+    ox_state = models.CharField(max_length=50, null=True, blank=True)
+    en = models.CharField(max_length=50, null=True, blank=True)
+
 
 
     def __str__(self):
