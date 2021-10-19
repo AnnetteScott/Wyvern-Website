012cedf1c49caddcd98fa457175cb0e27ae3e996
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-19 15:46:16 +1300

    Removed unused items

--- ChemHub/models.py
+++ ChemHub/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Element(models.Model):
-    name = models.CharField(max_length=50)
-    atomic_number = models.CharField(max_length=50)
-    mass = models.CharField(max_length=50)
-    symbol = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    melting = models.CharField(max_length=50)
-    boiling = models.CharField(max_length=50)
-    density = models.CharField(max_length=50)
-    electron = models.CharField(max_length=50)
