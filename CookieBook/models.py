50ed60f5b8c3c30eeddbe54b07b5f9a7c2aaf732
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-19 21:43:25 +1300

    Added CookieBook Model

--- CookieBook/models.py
+++ CookieBook/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=400, null=True, blank=True)
+    cooking_time = models.CharField(max_length=50)
+    ingredients = models.CharField(max_length=1000)
+    directions = models.CharField(max_length=2000)
+    tag = models.CharField(max_length=20, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
