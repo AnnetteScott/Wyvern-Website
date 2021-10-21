2a524b74cc1959d429854c11853caba5a1a0cb34
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-22 12:45:32 +1300

    Update urls.py

--- CookieBook/urls.py
+++ CookieBook/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [ 
     path('cookebook/', views.home, name="CookieBook"),
     path('cookebook/<slug:the_type_of_recipe>/', views.recipeList, name="RecipePage"),
-    path('cookebook/<slug:the_type_of_recipe>/<slug:recipename>/', views.recipePage, name="RecipePage"),
+    path('cookebook/<slug:the_type_of_recipe>/<slug:recipeurl>/', views.recipePage, name="RecipePage"),
 ]
