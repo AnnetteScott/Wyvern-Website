747410a95e374b8f0d130eb040422276f609de5d
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-21 23:14:46 +1300

    Update views.py

--- CookieBook/views.py
+++ CookieBook/views.py
@@ -1,40 +1,40 @@
 from django.shortcuts import render
 from . import functions
-recipe_type_list = functions.get_all_recipe_file_names()
-recipes_list = []
-filename = ""
+from .models import Recipe
+
 
 # Create your views here.
 def home(request):
-    global recipe_type_list 
-    recipe_type_list = functions.get_all_recipe_file_names()
+    current_types = []
+    recipe_list = list(Recipe.objects.values_list('tag', flat=True))
+    recipe_type_list = []
+    for type in recipe_list:
+        a_dict = {}
+        if type not in current_types:
+            url = type.lower()
+            a_dict['recipeType'] = url
+            a_dict['UpperCase'] = type
+            recipe_type_list.append(a_dict)
+            current_types.append(type)
+
     context = {'recipe_type': recipe_type_list} 
     return render(request, 'CookieBook/home.html', context)
 
 def recipeList(request, the_type_of_recipe):
-    type_of_recipe = None
-    for recipe_type in recipe_type_list:
-        if recipe_type['recipeType'] == str(the_type_of_recipe):
-            type_of_recipe = recipe_type
-    filename = type_of_recipe["UpperCase"]
-    global recipes_list
-    recipes_list = functions.get_recipe_dict(filename)
-    valid_letters = functions.check_alphabet(filename)
-    context = {'recipes': recipes_list, 'alphabet': valid_letters}
+    list_ids = list(Recipe.objects.values_list('id', flat=True).order_by('id'))
+    recipe_page_list = []
+    alphabet = []
+    for index in range(len(list_ids)):
+        recipe_obj = Recipe.objects.get(id=list_ids[index])
+        if recipe_obj.tag.lower() == str(the_type_of_recipe):
+            recipe_page_list.append(recipe_obj)
+            alphabet.append(recipe_obj.first_letter) 
+
+    context = {'alphabet': alphabet, 'recipe': recipe_page_list}
     return render(request, 'CookieBook/table_of_contents.html', context)
 
 
 def recipePage(request, the_type_of_recipe, recipename):
-    recipe = None
-    type_of_recipe = None
-    for recipe_type in recipe_type_list:
-        if recipe_type['recipeType'] == str(the_type_of_recipe):
-            type_of_recipe = recipe_type
-    filename = type_of_recipe["UpperCase"]
-    recipes_list = functions.get_recipe_dict(filename)
-    for dict_obj in recipes_list:
-        if dict_obj['url'] == str(recipename):
-            recipe = dict_obj
-    context = {'recipe': recipe}
+    context = {}
     return render(request, 'CookieBook/recipe.html', context)
 
