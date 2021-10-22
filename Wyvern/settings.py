7388d64fdf58775ff58e164d24045378dead58b7
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-22 16:09:57 +1300

    Update settings.py

--- Wyvern/settings.py
+++ Wyvern/settings.py
@@ -130,10 +130,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEIDA_ROOT = os.path.join(BASE_DIR, "static", "Images")
-MEDIA_URL = "/Images/"
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
