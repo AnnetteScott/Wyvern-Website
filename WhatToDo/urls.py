b10dfcc82cb591aae0a55487626ecf1928a4df39
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-22 20:15:58 +1300

    Update urls.py

--- WhatToDo/urls.py
+++ WhatToDo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [ 
     path('whattodo/', views.home, name="WhatToDo"),
+    path('whattodo/result/', views.result, name="WhatToDoResult"),
 ]
