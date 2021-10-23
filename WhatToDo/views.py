c8ecf2f5367a94305ad1882dfd6ed7fdc130a9d3
NotNatural21 <annettemorganscott@outlook.co.nz>
2021-10-23 14:43:34 +1300

    Update views.py

--- WhatToDo/views.py
+++ WhatToDo/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import render
 from .forms import UserInputForm
 import random
 
-form = None
-
-def result(request):
-    chosen = None
-    user_input = (request.POST.get('user_input')).split('\n')
-    random_num = random.randrange(0, len(user_input))
-    chosen = user_input[random_num]
-    context = {'chosen': chosen}
-    return render(request, 'WhatToDo/result.html', context )
-
-
 def home(request):
-    form = UserInputForm()
-    context = {'form': form}
+    chosen = None
+    button_value = None
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = UserInputForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():      
+            user_input = (request.POST.get('user_input')).split('\n')
+            random_num = random.randrange(0, len(user_input))
+            chosen = user_input[random_num]
+            button_value = "Resubmit"
+    else:
+        form = UserInputForm()
+        button_value = "Submit"
+
+    context = {'form': form, 'chosen': chosen, 'Button_Value': button_value}
     return render(request, 'WhatToDo/home.html', context)
