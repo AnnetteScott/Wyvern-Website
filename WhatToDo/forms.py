97e178952262f36ad454db3b6cce7c85afc75c8f
NotNatural21 <annettemorganscott@outlook.co.nz>
2021-10-23 14:45:32 +1300

    Update forms.py

--- WhatToDo/forms.py
+++ WhatToDo/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UserInputForm(forms.Form):
-    user_input = forms.CharField(label='Enter some things here', widget=forms.Textarea)
+    user_input = forms.CharField(label='Enter Each Item On a New Line:', widget=forms.Textarea)
