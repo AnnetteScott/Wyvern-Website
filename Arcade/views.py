bd339ff9c3c9d53a80c8690643c6e1f29605cad3
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-24 18:35:16 +1300

    Update views.py

--- Arcade/views.py
+++ Arcade/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import GamesList
 
 # Create your views here.
 def home(request):
-    return render(request, 'Arcade/home.html')
+    games_list_dict = list(GamesList.objects.values())
+    context = {'games_list': games_list_dict}
+    return render(request, 'Arcade/home.html', context)
+
