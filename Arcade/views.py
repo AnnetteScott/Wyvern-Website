21c9588ca4b39d34551de13319d1feb0d9a3b6c6
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-24 19:17:43 +1300

    Update views.py

--- Arcade/views.py
+++ Arcade/views.py
@@ -7,3 +7,22 @@ def home(request):
     context = {'games_list': games_list_dict}
     return render(request, 'Arcade/home.html', context)
 
+def gamePage(request, gameurl):
+    games_list_dict = list(GamesList.objects.values())
+    title = None
+    for game in games_list_dict:
+        url = ''
+        if game['url'] == '' or game['url'] == 'null':
+            url = (game['title'].replace(' ', '-')).lower()
+        if game['url'] == gameurl or url == gameurl:
+            title = game['title']
+            iframe_title = game['title'].replace(' ', '-')
+            game_status = game['online']
+            break
+
+    if game_status == True:
+        ifame_src = "https://notnatural21.github.io/" + iframe_title + "/"
+        context = {'title': title, 'ifame_src': ifame_src, 'status': game_status}
+        return render(request, 'Arcade/gamepage.html', context)
+    else:
+        return render(request, 'Arcade/gamedownpage.html')
