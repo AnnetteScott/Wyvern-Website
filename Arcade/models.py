2e9eff79c60eb2667fff92e354eed83b8801eff7
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-24 18:35:01 +1300

    Update models.py

--- Arcade/models.py
+++ Arcade/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class GamesList(models.Model):
+    title = models.CharField(max_length=100)
+    img = models.FileField(upload_to='static/Images/Arcade', null=True, blank=True)
+    online = models.BooleanField()
+    url = models.CharField(max_length=20, blank=True)
+
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
