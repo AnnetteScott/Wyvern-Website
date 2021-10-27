717ed810f754ee7fdeeec2e7e5d919607801c5eb
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-28 11:24:50 +1300

    Create process_score.py

---
+++ Arcade/Games/ExplodingKacti/process_score.py
@@ -0,0 +1,15 @@
+from ...models import ExplodingKactiScore
+
+def updateScores():        
+    data = list(ExplodingKactiScore.objects.values().order_by('-score'))
+    if len(data) > 10:
+        data_length = len(data)
+        for index in range(10, data_length):
+            obj_id = data[index]['id']
+            ExplodingKactiScore.objects.filter(pk=obj_id).delete()
+
+    
+
+
+
+
