7ebc95bf46c46dbbd2421a70ee105424bdbda99a
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-28 19:51:16 +1300

    Update urls.py

--- Arcade/urls.py
+++ Arcade/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [ 
     path('arcade/', views.home, name="Arcade"),
     path('arcade/<slug:gameurl>/', views.gamePage, name="ArcadeGame"),
-    path('arcade/<slug:gameurl>/add_score/', views.add_score),
+    path('arcade/exploding-kacti/add_score/', views.add_score),
 ]
