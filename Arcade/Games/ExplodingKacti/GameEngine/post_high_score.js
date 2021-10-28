bf3d0e7083f3dc465b91fa171eb9f98392446439
Giga <gigabyte5671@gmail.com>
2021-10-28 19:50:59 +1300

    Update post_high_score.js

--- Arcade/Games/ExplodingKacti/GameEngine/post_high_score.js
+++ Arcade/Games/ExplodingKacti/GameEngine/post_high_score.js
@@ -1,16 +1,12 @@
 var data_transmit_enable = 1;
-var begin_fetch_time;
 function transmitToServer(data){
   if(data_transmit_enable == 1){
     data_transmit_enable = 0;
-    begin_fetch_time = Date.now();
     $.ajax({
-      method: "POST",
+      method: 'POST',
       url: '/add_score/',
       data: data,
       success: function(){
-        const end_fetch_time = Date.now();
-        console.log('Data sent in ', ((end_fetch_time - begin_fetch_time) / 1000) + "s");
         data_transmit_enable = 1;
       },
       error: function(xhr, status, err){
