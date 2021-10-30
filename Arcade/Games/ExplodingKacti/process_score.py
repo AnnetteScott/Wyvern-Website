81f28f0cd1b900b2e596ef8c59c8f3efd0135d94
Giga <gigabyte5671@gmail.com>
2021-10-30 13:21:02 +1300

    Update process_score.py

--- Arcade/Games/ExplodingKacti/process_score.py
+++ Arcade/Games/ExplodingKacti/process_score.py
@@ -2,7 +2,7 @@ from ...models import ExplodingKactiScore
 
 def updateScores():
     data = list(ExplodingKactiScore.objects.values().order_by('-score'))        
-    if len(data) > 10:
+    if len(data) > 20:
         data_length = len(data)
         for index in range(10, data_length):
             obj_id = data[index]['id']
