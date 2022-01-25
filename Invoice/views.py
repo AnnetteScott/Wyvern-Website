e04847febb52500bc84f6c55b1fe585ff96d5953
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-25 18:31:43 +1300

    added redirection

--- Invoice/views.py
+++ Invoice/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse
-from django.contrib.auth import login, logout
 
 # Create your views here.
 def home(request):
-    return render(request, 'Invoice/home.html')
+    if request.user.is_authenticated:
+        url = "/invoice/" + request.user.username + "/"
+        return redirect(url)
+    else:
+        return render(request, 'Invoice/home.html')
     
 def userHome(request, user_name):
 
-    return render(request, 'Invoice/home.html')
+    return render(request, 'Invoice/userHome.html')
