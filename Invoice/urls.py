83f0d209bc9594fb9cd15612d90141f72e21d7a1
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-25 15:54:59 +1300

    Started on signup

--- Invoice/urls.py
+++ Invoice/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [ 
     path('invoice/', views.home, name="Invoice"),
+    path('invoice/signup/', views.SignUp, name="SignUp"),
+    path('invoice/signin/', views.SignIn, name="SignIn"),
 ]
--- Invoice/views.py
+++ Invoice/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'Invoice/home.html')
+    
+def SignUp(request):
+    form = UserCreationForm()
+    return render(request, 'Invoice/signup.html', {'form': form})
+    
+def SignIn(request):
+    return render(request, 'Invoice/signin.html')
