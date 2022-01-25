087c73922dcff3911790251c70bcb4d5835c3b78
Giga <annettemorganscott@outlook.co.nz>
2022-01-26 10:38:15 +1300

    Added timesheet url

--- Invoice/urls.py
+++ Invoice/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [ 
     path('invoice/', views.home, name="Invoice"),
     path('invoice/<slug:user_name>/', views.userHome, name="UserHome"),
+    path('invoice/<slug:user_name>/<slug:time_sheet>/', views.userHome, name="TimeSheet"),
 ]
