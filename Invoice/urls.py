fc2cdf1a073b972e0ca4ba3806bc0dd611db3fe0
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-25 16:51:09 +1300

    add ability to sign up

--- Invoice/urls.py
+++ Invoice/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [ 
     path('invoice/', views.home, name="Invoice"),
-    path('invoice/signup/', views.SignUp, name="SignUp"),
-    path('invoice/signin/', views.SignIn, name="SignIn"),
+    path('signup/', views.SignUp, name="SignUp"),
+    path('signin/', views.SignIn, name="SignIn"),
 ]
--- Invoice/views.py
+++ Invoice/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
     return render(request, 'Invoice/home.html')
     
 def SignUp(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log the user in
+            return redirect(reverse('Invoice'))
+    else:
+        form = UserCreationForm()
     return render(request, 'Invoice/signup.html', {'form': form})
-    
+
+
 def SignIn(request):
     return render(request, 'Invoice/signin.html')
