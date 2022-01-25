c3705477ae7362bafa8e7f9542f2a0eb1c285549
Giga <annettemorganscott@outlook.co.nz>
2022-01-26 10:37:54 +1300

    Created User Settings model

--- Invoice/models.py
+++ Invoice/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class UserSettings(models.Model):
+	user = models.CharField(max_length=100)
+	maxWeeks = models.IntegerField()
+	lastWeeks = models.IntegerField()
+	startDate = models.DateTimeField()
+
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return str(self.title)
